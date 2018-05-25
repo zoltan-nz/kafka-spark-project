a78a898f4e7a2c9059a2d5861e9d07717652dcec
Zoltan <zdebre@gmail.com>
2018-05-25 22:07:00 +1200

    Setup Maven module and clean up the cluster based Spark

--- SparkStreamer/src/main/java/nz/zoltan/SparkStreamer.java
+++ SparkStreamer/src/main/java/nz/zoltan/SparkStreamer.java
@@ -1,24 +1,19 @@
 package nz.zoltan;
 
-import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
-import org.apache.spark.sql.streaming.StreamingQueryListener;
 
-import java.util.Arrays;
-import java.util.Iterator;
 
-public class App {
+public class SparkStreamer {
 
   public static void main(String[] args) throws StreamingQueryException {
 
     SparkSession spark = SparkSession
       .builder()
-      .appName("JavaStructuredNetworkWordCount")
+      .appName("SparkStreamer")
       .config("spark.master", "local")
       .getOrCreate();
 
@@ -30,18 +25,6 @@ public class App {
       .option("subscribe", "boerse.dev")
       .load();
 
-
-//    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
-
-//    // Split the lines into words
-//    Dataset<String> words = df
-//      .as(Encoders.STRING())
-//      .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
-//
-//    // Generate running word count
-//    Dataset<Row> wordCounts = words.groupBy("value").count();
-//
-//    // Start running the query that prints the running counts to the console
     StreamingQuery query = df.writeStream()
       .outputMode("append")
       .format("console")
--- spark/README.md
+++
@@ -1,9 +0,0 @@
-# Spark
-
-Source: https://github.com/gettyimages/docker-spark
-
-```
-$ docker build spark
-$ docker run spark
-$ open http://localhost:8080
-```
